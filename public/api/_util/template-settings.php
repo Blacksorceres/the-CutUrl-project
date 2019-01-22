c2838088a3c98129a6c94e6a359690c5108487a3
aljaxus <aljaxus.dev@gmail.com>
2019-01-22 19:02:31 +0100

    renamed to not ignored name

---
+++ public/api/_util/template-settings.php
@@ -0,0 +1,9 @@
+<?php
+// Edit the credentials to suit your setup and rename the file to "settings.nogit.php"
+class mysqlAuth {
+	protected $dbhost = 'host';
+	protected $dbuser = 'user';
+	protected $dbpass = 'password';
+	protected $dbname = 'database_name';
+
+}
